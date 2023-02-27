29f5fb4e516d28475612ce00075282710eeae78f
lizastrappini <lizastrappini99@gmail.com>
2023-02-27 12:48:47 -0300

    prueba2

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,6 +144,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Configure Django App for Heroku.
-
-django_heroku.settings(locals())
