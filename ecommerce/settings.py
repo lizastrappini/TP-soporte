d13ec51ef6d0d3e748bd7ad459e2a1dd579e4198
lizastrappini <lizastrappini99@gmail.com>
2023-02-27 12:37:20 -0300

    prueba

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -143,3 +143,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
